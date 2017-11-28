d05c4a077e368bd74e1277576b59e9866c9ca9c7
Baptiste Robichon <baptisterobichon@laposte.net>
2017-11-28 17:10:40 +0100

    1

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: brobicho <brobicho@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/28 15:17:35 by brobicho     #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/28 16:09:37 by brobicho    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/11/28 17:09:52 by brobicho    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,9 +19,17 @@
 int		get_next_line(const ind fd, char **line)
 {
 	int i;
-	char buff[BUFF_SIZE];
+	static char *copy;
+	int ret;
+	char buff[BUFF_SIZE + 1];
 
+	if (fd < 0)
+		return (-1);
 	i = 0;
-	while ()
-	read(fd, &buff, 1);
+	if (!copy)
+	{
+		copy = (char*)malloc(BUFF_SIZE);
+		if (!((ret = read(fd, save, BUFF_SIZE)) < 1))
+			return (0);
+
 
